6edb24af9afe80b4d5b7fb712fd720193e19bc25
Rashed Al-Lahaseh <rallahaseh@gmail.com>
2023-11-03 11:39:35 +0300

    Add app color palette

---
+++ ChatBot/ChatBot/Utilities/Colors.swift
@@ -0,0 +1,25 @@
+//
+//  Colors.swift
+//  ChatBot
+//
+//  Created by Rashed Al-Lahaseh on 03/11/2023.
+//
+
+import SwiftUI
+
+struct Colors {
+    // Primary color.
+    static let primary = Color("primary")
+    
+    // Background color for the chat.
+    static let chatBackground = Color("chatBackground")
+    
+    // Background color for messages.
+    static let messageBackground = Color("messageBackground")
+    
+    // Text color for messages.
+    static let messageContent = Color("messageContent")
+    
+    // Title color.
+    static let title = Color("title")
+}
